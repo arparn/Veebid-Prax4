949c8150a091113fe9dad75441b60815c47388d8
ARTHUR-ALEKSANDER PÄRNOJA <arthurp200148@gmail.com>
2020-12-12 00:00:49 +0200

    Fix server, make 2 gamers connect and create game.

--- public/frontend.js
+++ public/frontend.js
@@ -1,20 +1,55 @@
 const API_URL = 'http://localhost:8000';
 
 let game_id;
+let p_name;
 
 function connect(name) {
-    let p_name = {name: name};
-    fetch(`${API_URL}/connect`, {
-        method: 'POST',
-        body: JSON.stringify(p_name),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }})
-        .then(response => response.text())
-        .then(id => game_id = id)
-        .catch(err => console.error(err))
+    fetch(`/connect/?name=${name}`)
+        .then(res => res.json())
+        .then(data => {
+            game_id = data.id
+            p_name = data.name
+            sessionStorage.setItem('id', game_id)
+            sessionStorage.setItem('name', p_name)
+            get_data()
+            check_if_begin(game_id)
+        })
 }
 
-function get_id() {
-    console.log(game_id);
+function check_if_begin(id) {
+    fetch(`/check/?id=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.check === true) {
+                location.href = data.url
+            } else {
+                setTimeout(() => {
+                    check_if_begin(id)
+                }, 2000)
+            }
+        })
 }
+
+function get_data() {
+    console.log(sessionStorage.getItem('id'))
+    console.log(sessionStorage.getItem('name'))
+}
+
+
+
+
+
+
+// function connect(name) {
+//     let p_name = {name: name};
+//     fetch(`/connect`, {
+//         method: 'POST',
+//         body: JSON.stringify(p_name),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8'
+//         }})
+//         .then(response => response.json())
+//         .then(id => console.log(id.id))
+//         .catch(err => console.error(err))
+//
+// }
--- server.js
+++ server.js
@@ -1,40 +1,65 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const path = require('path')
+const port = 8000;
+const app = express();
+const bodyParser = require("body-parser");
 
-const app = express()
-const port = 8000
+app.use(express.static(path.join(__dirname, "public")))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
 
 let game_id = 0;
 let games = [];
 let players = [];
 
 
-app.use(cors())	//Enable CORS
-
-app.use(bodyParser.json()) // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
-
-app.post('/connect', ((req, res) => {
-    let name = req.body.name;
+app.get('/connect', function(req, res) {
+    let name = req.query['name']
     if (players.length <= 2) {
         players.push(name);
     }
     if (players.length === 1) {
-        games.push({id: 0, p1: '', p2: '', begin: false, turns: 13, whose_turn: 1});
+        games.push({id: game_id, p1: '', p2: '', begin: false, turns: 13, whose_turn: 1});
         games[game_id].p1 = name;
-        res.json({id: game_id});
+        res.send({id: game_id, name: name});
     } else if (players.length === 2) {
         games[game_id].p2 = name;
-        games[game_id].id = game_id;
         games[game_id].begin = true;
         players = [];
-        res.json({g_id: games[game_id].id});
         game_id ++;
+        res.send({id: games[game_id - 1].id, name: name});
     }
-    console.log(games);
-    console.log(game_id);
-}))
+     console.log(games)
+     console.log(game_id)
+})
+
+app.get('/check', function (req, res) {
+    let id = req.query['id']
+    return res.send({check: games[id].begin, url: '/multiplayer.html'})
+})
+
+
+
+// app.post('/connect', ((req, res) => {
+//     let name = req.body.name;
+//     if (players.length <= 2) {
+//         players.push(name);
+//     }
+//     if (players.length === 1) {
+//         games.push({id: 0, p1: '', p2: '', begin: false, turns: 13, whose_turn: 1});
+//         games[game_id].p1 = name;
+//         res.send({id: game_id});
+//     } else if (players.length === 2) {
+//         games[game_id].p2 = name;
+//         games[game_id].id = game_id;
+//         games[game_id].begin = true;
+//         players = [];
+//         res.send(false);
+//         game_id ++;
+//     }
+//     console.log(games);
+// }))
 
 app.listen(port, () => {
     console.log(`Listening at port: ${port}`)
